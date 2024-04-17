4d03c08ee7304697ec8f5c7d3cc44f3c60ebf66c
Roï Eielts <103494921+Roi-Eielts@users.noreply.github.com>
2024-04-17 21:13:41 +0200

    added readme en tussen commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# voordat je het project kan runnen moet je de volgende commands runnen:
+
+```
+npm install expo
+
+npx expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+npm install react-navigation
+npm install @react-navigation/native @react-navigation/native-stack
+npx expo install react-native-screens react-native-safe-area-context
+npm install @react-native-picker/picker --save
+```
+
+**voor in het web**
+
+let op! niet alle features werken in het web
+```
+npx expo install react-native-web react-dom @expo/metro-runtime
+```
--- src/module/pages/CreatePage.jsx
+++ src/module/pages/CreatePage.jsx
@@ -1,7 +1,26 @@
 const CreateProduct  = () => {
+    const [name, setName] = useState('');
+    const [barCode, setBarcode] = useState('');
+    const [ammount, setAmmoumt] = useState('');
+
+
     return (
         <View>
-            <Text>Create Page</Text>
+            <TextInput
+                    placeholder='Product naam'
+                    onChangeText={(text) => { setName(text) }}
+                    value={name}
+                />
+                 <TextInput
+                    placeholder='Product hoeveelheid'
+                    onChangeText={(text) => { setAmmoumt(text) }}
+                    value={name}
+                />
+                 <TextInput
+                    placeholder='Product naam'
+                    onChangeText={(text) => { setName(text) }}
+                    value={name}
+                />
         </View>
     );
 }
